01b961101c492a5ba7d4c02375a6cd8b43cf6631
Elton Luiz <eltonluiz12042012@gmail.com>
2022-09-05 19:40:49 -0300

    Tabela de usuários, controller e model

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -6,6 +6,13 @@ use App\Controllers\BaseController;
 
 class Usuarios extends BaseController
 {
+    private $usuarioModel;
+
+    public function __construct()
+    {
+        $this->usuarioModel = new \App\Models\UsuarioModel();
+    }
+
     public function index()
     {
         //
--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -6,37 +6,30 @@ use CodeIgniter\Model;
 
 class UsuarioModel extends Model
 {
-    protected $DBGroup          = 'default';
     protected $table            = 'usuarios';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = true;
+    protected $allowedFields    = [
+        'nome',
+        'email',
+        'password',
+        'reset_hash',
+        'reset_expira_em',
+        'imagem',
+    ];
 
     // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $useTimestamps = true;
+    protected $createdField  = 'criado_em';
+    protected $updatedField  = 'atualizado_em';
+    protected $deletedField  = 'deletado_em';
 
     // Validation
     protected $validationRules      = [];
     protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+
 
     // Callbacks
-    protected $allowCallbacks = true;
     protected $beforeInsert   = [];
-    protected $afterInsert    = [];
     protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
